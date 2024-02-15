4e0e1b84ea447c71d37782d5ccadb691b86222bb
Sudarshan Adhikari <sdadkry95@gmail.com>
2024-02-15 12:00:22 +0545

    form values

--- src/day2/child.jsx
+++ src/day2/child.jsx
@@ -2,20 +2,46 @@ import { useState } from "react";
 
 export default function Child() {
   const [sum, setSum] = useState(0);
+  const [first, setFirst] = useState(0);
+  const [second, setSecond] = useState(0);
 
   // sum state
-  function calculateStm(a,b) {
-    //save sum in state
-    setSum(a+b);
+  function calculateSum() {
+    setSum(Number(first) + Number(second));
   }
-  function increment() {
-    setSum(sum + 1);
+
+  function setValueForFirstItem(event) {
+    setFirst(event.target.value);
+  }
+
+  function setValueForSecondItem(event) {
+    setSecond(event.target.value);
   }
 
   return (
     <div>
+      <div>
+        <label htmlFor="first">Enter Number 1</label>
+        <input
+          id="first"
+          name="first"
+          type="number"
+          value={first}
+          onChange={setValueForFirstItem}
+        />
+      </div>
+      <div>
+        <label htmlFor="second">Enter Number 2</label>
+        <input
+          id="second"
+          name="second"
+          type="number"
+          value={second}
+          onChange={setValueForSecondItem}
+        />
+      </div>
+      <button onClick={() => calculateSum()}>Add</button>
       <h1>sum - {sum}</h1>
-      <button onClick={() => calculateStm(2,2)}>Click</button>
     </div>
   );
 }
