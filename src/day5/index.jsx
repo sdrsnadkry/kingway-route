4eb1cf4b9306ca7ec813d37916972aa7c7c21e25
Sudarshan Adhikari <sdadkry95@gmail.com>
2024-02-20 13:57:22 +0545

    fix: added rating

--- src/day5/index.jsx
+++ src/day5/index.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 
 import axios from "axios";
+import Star, { EmptyStar } from "./star";
 
 const Day5 = () => {
   const [todos, setTodos] = useState([]);
@@ -16,15 +17,30 @@ const Day5 = () => {
     fetchDataFromApi();
   }, []);
 
+  const calculateStar = (rating) => {
+    const starCount = Math.floor(rating?.rate);
+
+
+    const array = Array(starCount).fill(Math.random());
+
+    return array;
+  };
+
   return (
     <div>
       Products List
       {todos?.map((item, index) => (
         <div key={item?.id}>
-          <img src={item?.image} alt="" srcset="" width={150} height={150} />
+          <img src={item?.image} alt=""   width={150} height={150} />
           <p>
             {index + 1} - {item?.title}
           </p>
+
+          {calculateStar(item?.rating)?.map((rating, index) => (
+            <Star key={index} />
+          ))}
+
+          {/* <EmptyStar/> */}
         </div>
       ))}
       <p></p>
---
+++ src/day5/star.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Star = () => {
+  return <div>&#9733;</div>;
+};
+
+export const EmptyStar = () => {
+  return <div>&#9734;</div>;
+};
+
+export default Star;
