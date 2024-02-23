dab142ee02e59a1193807e7aaccfce830373b5f3
Sudarshan Adhikari <sdadkry95@gmail.com>
2024-02-23 13:53:08 +0545

    feat: added validation on form

--- src/routes/login/index.jsx
+++ src/routes/login/index.jsx
@@ -1,6 +1,14 @@
 import React from "react";
 
 import { useFormik } from "formik";
+import { object, string } from "yup";
+
+const validationSchema = object({
+  email: string()
+    .required("Please enter your email")
+    .email("Pleas enter a valid email"),
+  password: string().required("Please enter your password"),
+});
 
 const Login = () => {
   const formik = useFormik({
@@ -8,10 +16,15 @@ const Login = () => {
       email: "",
       password: "",
     },
+    validationSchema: validationSchema,
     onSubmit: (values) => {
       console.log(values);
+
+      formik?.resetForm()
     },
-  }); 
+  });
+
+  console.log(formik);
 
   return (
     <div className="wrapper">
@@ -21,14 +34,17 @@ const Login = () => {
           <input
             type="email"
             name="email"
-            required
             value={formik.values.email}
             onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
             className="form-control"
             id="exampleInputEmail1"
             aria-describedby="emailHelp"
             placeholder="Enter email"
           />
+          {formik?.touched?.email && (
+            <small className="text-danger">{formik?.errors?.email}</small>
+          )}
         </div>
         <div className="form-group mt-3">
           <label htmlFor="exampleInputPassword1">Password</label>
@@ -36,11 +52,15 @@ const Login = () => {
             type="password"
             name="password"
             onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
             value={formik.values.password}
             className="form-control"
             id="exampleInputPassword1"
             placeholder="Password"
           />
+          {formik?.touched?.password && (
+            <small className="text-danger">{formik?.errors?.password}</small>
+          )}
         </div>
 
         <div className="mt-3">
